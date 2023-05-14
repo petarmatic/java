4198256c8035abf6a6826fff6247909cc52c5434
perom00 <petarmatic00@gmail.com>
2023-05-14 18:22:11 +0200

    Update vrtic.sql

--- vrtic.sql
+++ vrtic.sql
@@ -3,21 +3,21 @@ CREATE DATABASE vrtic;
 use vrtic;
 
 CREATE TABLE osoba(
-    sifra int not null PRIMARY KEY,
+    sifra int not null PRIMARY key auto_increment,
     ime VARCHAR(20),
     prezime VARCHAR(20),
     oib CHAR(20)
 );
 
 CREATE TABLE odgajateljica(
-    sifra int not null PRIMARY KEY,
+    sifra int not null PRIMARY key auto_increment,
     osoba INT,
     iban char(15),
     kontakt char(15)
 );
 
 CREATE TABLE dijete(
-    sifra INT NOT NULL PRIMARY KEY,
+    sifra INT NOT NULL PRIMARY key auto_increment,
     osoba INT,
     odgojna_skupina int,
     odgajateljica int,
@@ -25,15 +25,15 @@ CREATE TABLE dijete(
 );
 
 CREATE table odgojna_skupina(
-    sifra int not null PRIMARY key,
-    odgajateljica not NULL,
-    dijete int,
+    sifra int not null PRIMARY key auto_increment,
+    odgajateljica int,
+    dijete int
 );
 
 CREATE table djecji_vrtic(
-    sifra int not null PRIMARY KEY,
-    odgajateljica not NULL,
-    odgojnaskupina varchar(44),
+    sifra int not null PRIMARY key auto_increment,
+    odgajateljica int,
+    odgojnaskupina int,
     dijete int not NULL,
     adresa VARCHAR(50),
     radnovrijeme datetime,
@@ -42,24 +42,38 @@ CREATE table djecji_vrtic(
 );
 
 CREATE TABLE roditelj(
-    sifra int not null primary key,
+    sifra int not null primary key auto_increment,
     osoba int not null,
     kontakt char(15),
     email varchar(20)
 );
 
 ALTER table odgajateljica add FOREIGN KEY (osoba) REFERENCES osoba(sifra);
-ALTER table odgojnaskupina ADD FOREIGN KEY (odgajateljica) REFERENCES dijete(sifra);
+ALTER table odgojna_skupina ADD FOREIGN KEY (odgajateljica) REFERENCES dijete(sifra);
 ALTER TABLE djecji_vrtic add FOREIGN KEY (odgajateljica) references odgajateljica(sifra);
 ALTER TABLE djecji_vrtic ADD FOREIGN KEY (dijete) REFERENCES dijete(sifra);
 ALTER table djecji_vrtic ADD FOREIGN KEY (odgojnaskupina) REFERENCES odgojna_skupina(sifra);
 ALTER table dijete add FOREIGN KEY (roditelj) REFERENCES roditelj(sifra);
 
-INSERT into osoba (ime, prezime, oib) VALUES ('Mirna', 'Miric', '515165151');
-INSERT into odgajateljica (osoba) VALUES ('Mima', 'Horvat', '4616565');
-INSERT into odgajateljica (iban, kontakt) VALUES ("HR11114512", "09155151");
-INSERT into dijete (osoba) VALUES ("Marija", "Horvat", "111515");
-INSERT into dijete (odgojna_skupina, odgajateljica, roditelj) values ("misevi", "Mima", "Marta");
-INSERT into odgojna_skupina (odgajateljica, dijete) VALUES ("Mima", "Marija");
-INSERT INTO djecji_vrtic (odgajateljica, odgojnaskupina, dijete, adresa, radnovrijeme, naziv, kapacitet) VALUES ("Mima", "misevi", "Marija", "osijek", "2.2", "sunce", "26");
-INSERT INTO roditelj (osoba, kontakt, email) values ("Marta", "311892", "martamail");
+INSERT into osoba (ime, prezime, oib) VALUES (1,'Marija', 2,'Miric', 3,'515165151',);
+
+INSERT into dijete (osoba) VALUES (1, 'Marija');
+INSERT into dijete (odgojna_skupina) values (2,'misevi');
+INSERT into dijete (odgajateljica) values (3, 'Mima');
+INSERT into dijete (roditelj) values (4, 'Marta');
+
+INSERT into odgajateljica (osoba) VALUES (1, 'Mima');
+INSERT into odgajateljica (iban) VALUES (1, '4616565');
+INSERT into odgajateljica (kontakt) VALUES (1, "09155151");
+INSERT into odgojna_skupina (odgajateljica) VALUES (1, 'Mima');
+INSERT into odgojna_skupina (dijete) VALUES (1, 'Marija');
+
+INSERT INTO djecji_vrtic (odgajateljica) VALUES (1, 'Mima');
+INSERT INTO djecji_vrtic (odgojnaskupina) VALUES (1, 'misevi');
+INSERT INTO djecji_vrtic (dijete) VALUES (1, 'Marija');
+INSERT INTO djecji_vrtic (adresa) VALUES (1, 'osijek');
+INSERT INTO djecji_vrtic (radnovrijeme) VALUES (1, '2.2');
+INSERT INTO djecji_vrtic (naziv) VALUES (1, 'sunce');
+INSERT INTO djecji_vrtic (kapacitet) VALUES (1, '26');
+
+INSERT INTO roditelj (osoba, kontakt, email) values (1, 'Marta', '311892', 'martamail');
