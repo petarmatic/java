bf65f99bee26af22e43364bcc54bc32980eb4925
perom00 <petarmatic00@gmail.com>
2023-05-25 22:53:30 +0200

    Update edunovajp28.sql

--- edunovajp28.sql
+++ edunovajp28.sql
@@ -169,4 +169,66 @@ UPDATE osoba set prezime='Novoselić' where sifra=5;
 -- delete from predavac where osoba=1;
  -- delete from grupa where naziv='JP28';
 
+ # do sada smo radili
+select * from smjer;
+# minimalna forma select naredbe
+select 1;
+# filtriranje kolona
+select naziv, cijena from smjer;
+select naziv, sifra, naziv from smjer;
+select *, naziv from smjer;
+# select lista
+# 1. * sve kolone
+# 2. naziv kolone
+# 3. konstanta
+select naziv, 'Osijek' from smjer;
+# svakoj koloni se može dati zamjensko ime
+select naziv as smjer, 'Osijek' as grad from smjer;
+# 4. izraz
+select naziv, now() as datum, 2 as velicina, 66.22 as cijena from smjer;
+# ispišite imena i prezimena osoba
+select ime, prezime from osoba;
+# filtriranje redova
+select * from osoba;
+# koristim operatore
+# 1. uperatori uspoređivanja:
+# = < > <= >= !=
+select * from osoba where sifra=1;
+select * from osoba where sifra>=10;
+# 2. logički operatori - bool-ovi operatori
+select * from osoba where sifra>1 and sifra<3;
+select * from osoba where sifra=2 or sifra=3;
+select * from osoba where not (sifra>3 and sifra<9);
+# Izlistajte mi imena i prezimena svih osoba
+# koje se ne zovu Antonio
+select ime, prezime from osoba where ime!='Antonio'; 
+select ime, prezime from osoba where not ime='Antonio';
+# 3. ostali operatori
+# like
+select * from osoba where ime like 'A%';
+select * from osoba where ime like '%a';
+-- google način pretraživanja
+select * from osoba where ime not like '%a%';
+# between - upogoni logički and s uključenim vrijednostima
+select * from osoba where sifra>=3 and sifra<=5;
+select * from osoba where sifra between 3 and 5;
+# in - upogoni logički or
+select * from osoba where sifra=2 or sifra=6 or sifra=9;
+select * from osoba where sifra in (2,6,9);
+# postavite sebi svoj OIB
+update osoba set oib='61348493740'
+where sifra=1;
+# Izlistajte sve osobe koje imaju oib
+# operatori is null i is not null
+select * from osoba where oib is not null;
+# Unesite osobu Shaquille O'Neal
+# novije verzije baza dozvoljavaju "
+insert into osoba (ime,prezime)
+values ("Shaquille","O'Neal");
+select * from osoba where ime like 'S%';
+# sve verzije baza
+insert into osoba (ime,prezime)
+values ('Shaquille','O\'Neal');
+
+
 
