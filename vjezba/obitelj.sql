6597607f7daa54ac7b79f1ef66a3a73135ec3f71
perom00 <petarmatic00@gmail.com>
2023-06-07 13:08:06 +0200

    F.K., insert, update,delete,select

--- vjezba/obitelj.sql
+++ vjezba/obitelj.sql
@@ -12,6 +12,15 @@ CREATE table sestra(
     narukvica int not null
 );
 
+CREATE table muskarac(
+    sifra int not null PRIMARY KEY auto_increment,
+    bojaociju varchar(50) not null,
+    hlace varchar(30),
+    modelnaocala varchar(43),
+    maraka decimal(14,5) not null,
+    zena int
+);
+
 CREATE table zena(
     sifra int not null PRIMARY key auto_increment,
     treciputa datetime,
@@ -23,14 +32,6 @@ CREATE table zena(
     sestra int not NULL 
 );
 
-CREATE table muskarac(
-    sifra int not null PRIMARY KEY auto_increment,
-    bojaociju varchar(50) not null,
-    hlace varchar(30),
-    modelnaocala varchar(43),
-    maraka decimal(14,5) not null,
-    zena int
-);
 
 create table mladic(
     sifra int not NULL PRIMARY key auto_increment,
@@ -77,16 +78,51 @@ create table cura (
     punac int
 );
 
-insert into muskarac(bojaociju,hlace,modelnaocala,maraka,zena) VALUES('zelene','kratke','policajke','20',1);
-insert into muskarac(bojaociju,hlace,modelnaocala,maraka,zena) VALUES('crne','duge','pepeljarke','200',2);
-insert into muskarac(bojaociju,hlace,modelnaocala,maraka,zena) VALUES('smede','trenirka','nove','210',3);
+alter table sestra_svekar add FOREIGN key(sestra) REFERENCES sestra(sifra);
+alter table zena add FOREIGN key(sestra) REFERENCES sestra(sifra);
+alter table muskarac add FOREIGN key(zena) REFERENCES zena(sifra);
+alter table sestra_svekar add FOREIGN key(svekar) REFERENCES svekar(sifra);
+alter table mladic add FOREIGN key(muskarac) REFERENCES muskarac(sifra);
+alter table cura add FOREIGN key(punac) REFERENCES punac(sifra);
 
-INSERT into zena(treciputa,hlace,kratkamajica,jmbag,bojaociju,haljina,sestra) VALUES('2023-2-2', 'duge','plava','85514789315','crne','moderna',1);
+insert into sestra(introvertno, haljina,maraka,hlace,narukvica) values('1','plava','12,2','plave','kubanski vez');
+insert into sestra(introvertno, haljina,maraka,hlace,narukvica) values('1','zelena','14,2','zelene','hrvatski vez');
+insert into sestra(introvertno, haljina,maraka,hlace,narukvica) values('0','smeđa','18,2','smeđe','talijanski vez');
+
+INSERT into zena(treciputa,hlace,kratkamajica,jmbag,bojaociju,haljina,sestra) VALUES('2023-2-2', 'dugana','plava','85514789315','crne','moderna',1);
 INSERT into zena(treciputa,hlace,kratkamajica,jmbag,bojaociju,haljina,sestra) VALUES('2023-4-2', 'kratke','zelena','85599789315','ljubičaste','stara',2);
 INSERT into zena(treciputa,hlace,kratkamajica,jmbag,bojaociju,haljina,sestra) VALUES('2023-8-2', 'poderane','crvena','85514742315','žute','na točkice',3);
+
+INSERT INTO muskarac (bojaociju, hlace, modelnaocala, maraka, zena) VALUES ('zelene', 'kratke', 'policajke', '20', 1);
+INSERT INTO muskarac (bojaociju, hlace, modelnaocala, maraka, zena) VALUES ('crne', 'duge', 'pepeljarke', '200', 2);
+INSERT INTO muskarac (bojaociju, hlace, modelnaocala, maraka, zena) VALUES ('smede', 'trenirka', 'nove', '210', 3);
+
+insert into svekar(bojaociju,prstena,dukserica,lipa,eura,majica) VALUES ('plava','okrugli','nike','12,2','12,3','nike');
+insert into svekar(bojaociju,prstena,dukserica,lipa,eura,majica) VALUES ('zelena','srebrni','adidas','14,2','15,3','adidas');
+insert into svekar(bojaociju,prstena,dukserica,lipa,eura,majica) VALUES ('žuta','zlatni','vans','18,2','18,3','vans');
 
 INSErt into sestra_svekar(sestra,svekar) values(1,1);
 INSErt into sestra_svekar(sestra,svekar) values(2,2);
 INSErt into sestra_svekar(sestra,svekar) values(3,3);
 
-insert into sestra values()
+update cura set gustoca='15,77';
+
+DELETE FROM mladic WHERE kuna > 15.78;
+
+select kratkamajica from zena where hlace like '%ana%';
+
+select f.dukserica, a.asocijalno, b.hlace
+from mladic a
+inner join muskarac b 		on b.sifra=a.muskarac 
+inner join zena c 			on c.sifra=b.zena
+inner join sestra d 		on d.sifra=c.sestra 
+inner join sestra_svekar e 	on d.sifra=e.sestra
+inner join svekar f 		on f.sifra=e.svekar 
+where c.hlace like 'a%' and d.haljina like '%ba%'
+order by b.hlace desc;
+
+select a.haljina, a.maraka
+from sestra a
+left join sestra_svekar b on a.sifra=b.sestra
+where b.sestra is null;
+
