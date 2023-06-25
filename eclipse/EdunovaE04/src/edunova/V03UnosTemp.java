f30e2c522dd6d4d299b573adf98d02cf53f8e5d3
perom00 <petarmatic00@gmail.com>
2023-06-25 15:26:59 +0200

    Create V03UnosTemp.java

---
+++ eclipse/EdunovaE04/src/edunova/V03UnosTemp.java
@@ -0,0 +1,15 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class V03UnosTemp {
+
+	public static void main(String[] args) {
+		
+		
+		int i=Integer.parseInt(JOptionPane.showInputDialog("Unesi temp u C:"));
+		System.out.println("Temp u K: " + (i+273.15));
+		
+	}
+
+}
