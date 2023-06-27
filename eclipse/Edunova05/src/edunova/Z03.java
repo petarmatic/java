8b58124d5558baab16f254c1ff81c904de122583
perom00 <petarmatic00@gmail.com>
2023-06-27 12:36:54 +0200

    Update Z03.java

--- eclipse/Edunova05/src/edunova/Z03.java
+++ eclipse/Edunova05/src/edunova/Z03.java
@@ -9,13 +9,18 @@ public class Z03 {
 		// Unose se brojevi s while petljom u niz
 		// ispisuju se uneseni brojevi iz niza s foreach petljom
 		
+		int velicinaNiza=Integer.parseInt(JOptionPane.showInputDialog("unesi niz"));
+		int [] nizBrojeva= new int [velicinaNiza];
+		int brojac=0;
 		
 		
+		while(brojac < velicinaNiza) {
+			nizBrojeva[brojac++]=Integer.parseInt(JOptionPane.showInputDialog("unesi vrijednosti u niz"));
+			
+		
+	}
+		for( int en : nizBrojeva) {
+			System.out.println(en);
 		}
-		
 		}
-		
-		
-		
-
-
+}
