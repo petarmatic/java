17265f5d6991262d68ef501f53c5b1579bdbee71
perom00 <petarmatic00@gmail.com>
2023-07-03 18:06:16 +0200

    ?

--- eclipse/Edunova05/src/edunova/E04CM.java
+++ eclipse/Edunova05/src/edunova/E04CM.java
@@ -5,35 +5,37 @@ import javax.swing.JOptionPane;
 public class E04CM {
 
 	public static void main(String[] args) {
-		int redovi=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
-		int stupci=Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
-		
-		int [][] matrica= new int [redovi][stupci];
-		
-		
-		 int b=1;
-		 
-		 for(int i=1;i<=redovi;i++)
-				matrica[redovi-1][stupci-i]=b++;
-		 
-//		 matrica[redovi-2][0]=6;
-//		 matrica[redovi-3][0]=7;
-//		 matrica[redovi -4][0]=8;
-		 
-		 
-		 for(int i=2;i<=stupci;i++)
-			 matrica[redovi-i][0]=b++;
+		int redovi = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
+		int stupci = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
 
-		for(int i=0;i<redovi;i++) {
-			for(int j=0;j<stupci;j++) {
-				System.out.print(matrica[i][j] +"\t");
+		int[][] matrica = new int[redovi][stupci];
+
+		int v = 1;
+		int minred = 0;
+		int maxred = redovi - 1;
+
+		int minstup = 0;
+		int maxstup = stupci - 1;
+
+		while (v <= redovi * stupci) {
+			for (int i = minred; i <= maxred; i++) {
+				matrica [i][0]=v++;
+			}
+			
+		}
+			
+
+		
+		
+
+		for (int i = 0; i < redovi; i++) {
+			for (int j = 0; j < stupci; j++) {
+				System.out.print(matrica[i][j] + "\t");
 			}
 			System.out.println();
 		}
-		
-		
-			
-	}
 		}
+	}
+
 
 
