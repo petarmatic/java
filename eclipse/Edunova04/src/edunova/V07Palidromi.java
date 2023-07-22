42d03e1ec68b91dcac9e1a8bca8b24a37f05c316
perom00 <petarmatic00@gmail.com>
2023-07-22 20:52:28 +0200

    update Palidormi

--- eclipse/Edunova04/src/edunova/V07Palidromi.java
+++ eclipse/Edunova04/src/edunova/V07Palidromi.java
@@ -4,19 +4,35 @@ import javax.swing.JOptionPane;
 
 public class V07Palidromi {
 
-	public static void main(String[] args) {
-		int g=Integer.parseInt(JOptionPane.showInputDialog("gornji broj"));
-		int d=Integer.parseInt(JOptionPane.showInputDialog("donji broj"));
-		
-		
-		for(int i=g;i<=d;i++);
-		
-		while (i>0)
-			
-			
-		}
-			
+	 public static void main(String[] args) {
+	        int g = Integer.parseInt(JOptionPane.showInputDialog("gornji broj"));
+	        int d = Integer.parseInt(JOptionPane.showInputDialog("donji broj"));
+
+	        for (int i = g; i <= d; i++) {
+	            if (jePalindrom(i)) {
+	                System.out.println(i + " je palindrom.");
+	            }
+	        }
+	    }
+
+	    public static boolean jePalindrom(int broj) {
+	        int praviBroj = broj;
+	        int obrnutiBroj = 0;
+	        while (broj > 0) {
+	            int digit = broj % 10;
+	            obrnutiBroj = obrnutiBroj * 10 + digit;
+	            broj /= 10;
+	        }
+	        return praviBroj == obrnutiBroj;
+	    }
 	}
+	
+
+			
+			
+
+			
+
 
 
 
