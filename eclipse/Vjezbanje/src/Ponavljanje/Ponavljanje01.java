b2c96d39d63d31d07c674dd7c60c1fd3c5d65c85
perom00 <petarmatic00@gmail.com>
2023-07-24 22:27:23 +0200

    Create Ponavljanje01.java

---
+++ eclipse/Vjezbanje/src/Ponavljanje/Ponavljanje01.java
@@ -0,0 +1,29 @@
+package Ponavljanje;
+
+public class Ponavljanje01 {
+	
+	// ispiši brojeve
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+			//System.out.println(i);
+			System.out.print(i+ " ");
+		System.out.println();
+		
+		
+		for(int i=1;i<=10;i--)
+			System.out.print(i + " ");
+		System.out.println();
+		
+		
+		
+		
+		// ispis parnih
+//		FOR(INT I=1;I<=20;I++)
+//			SYSTEM.OUT.PRINTLN(I+" ");
+	}
+}
+	
+
+
+
