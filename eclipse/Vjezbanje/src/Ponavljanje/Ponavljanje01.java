e91bef79413bcdee904113d12aa17f58f66c0a8b
perom00 <petarmatic00@gmail.com>
2023-07-26 12:47:29 +0200

    Update Ponavljanje01.java

--- eclipse/Vjezbanje/src/Ponavljanje/Ponavljanje01.java
+++ eclipse/Vjezbanje/src/Ponavljanje/Ponavljanje01.java
@@ -1,5 +1,7 @@
 package Ponavljanje;
 
+import java.util.Scanner;
+
 public class Ponavljanje01 {
 	
 	// ispiši brojeve
@@ -11,7 +13,7 @@ public class Ponavljanje01 {
 		System.out.println();
 		
 		
-		for(int i=1;i<=10;i--)
+		for(int i=10;i>=1;i--)
 			System.out.print(i + " ");
 		System.out.println();
 		
@@ -19,8 +21,51 @@ public class Ponavljanje01 {
 		
 		
 		// ispis parnih
-//		FOR(INT I=1;I<=20;I++)
-//			SYSTEM.OUT.PRINTLN(I+" ");
+		for(int i=1;i<=10;i++)
+			if(i %2 ==0)
+				System.out.print(i + " ");
+		System.out.println();
+		
+		
+		for(int i=1;i<=10;i++)
+			if(i %2 !=0)
+				System.out.print(i + " ");
+		System.out.println();
+		
+		
+		for(int i=33;i>=3;i--)
+			if(i%3==0)
+				System.out.print(i +" ");
+		System.out.println();
+		
+//		scanner scanner= new scanner(system.in);
+//		
+//		system.out.print("unesi prvi broj");
+//		int broj1=scanner.nextint();
+//		
+//		system.out.print("unesi drugi broj");
+//		int broj2=scanner.nextint();
+//	  
+//		system.out.print("prirodni brojevi koji su između" + " " +broj1 + "i" +" " + broj2 +" " + "su:" );
+//		
+//		for(int i=broj1;i<=broj2;i++)
+//			system.out.print(i + " ");
+//		
+		Scanner scanner2=new Scanner(System.in);
+		
+		System.out.println("Unesi broj neki");
+		int broj=scanner2.nextInt();
+		System.out.println("Prirodni brojevi od 1" + " " + "do" + " "  + broj + " "+ "su:");
+		for(int i=broj;i>=1;i--)
+			System.out.print(i + " ");
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
 	
