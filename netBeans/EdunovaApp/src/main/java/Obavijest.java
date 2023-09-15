665f6a3272ba5b3a57d2b103bea7620dfc9834ce
perom00 <petarmatic00@gmail.com>
2023-09-15 11:03:32 +0200

    slack ne radi

---
+++ netBeans/EdunovaApp/src/main/java/Obavijest.java
@@ -0,0 +1,39 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class Obavijest {
+    public static void posaljiPorukuOGresci() {
+        String webhookUrl = "https://hooks.slack.com/services/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"; // Postavite svoj Webhook URL ovdje
+        String porukaOGresci = "Došlo je do greške u aplikaciji!";
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(webhookUrl);
+
+            // Postavite JSON tijelo zahtjeva sa informacijama o grešci
+            String jsonPayload = "{\"text\":\"" + porukaOGresci + "\"}";
+            StringEntity entity = new StringEntity(jsonPayload);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Content-Type", "application/json");
+
+            // Izvršite zahtjev
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                HttpEntity responseEntity = response.getEntity();
+                if (responseEntity != null) {
+                    System.out.println("Obavijest poslana na Slack!");
+                    EntityUtils.consume(responseEntity);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        posaljiPorukuOGresci();
+    }
+}
--- netBeans/EdunovaApp/src/main/java/edunova/controller/ObradaGrupa.java
+++ netBeans/EdunovaApp/src/main/java/edunova/controller/ObradaGrupa.java
@@ -35,5 +35,6 @@ import java.util.List;
         
     }
     }
+
     
 
