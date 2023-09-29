630a2c3a8766d2de4cab8b508766a0341e6eaed5
perom00 <petarmatic00@gmail.com>
2023-09-29 22:01:45 +0200

    a

--- eclipse/Edunova06/src/edunova/LK.java
+++ eclipse/Edunova06/src/edunova/LK.java
@@ -1,77 +1,12 @@
 package edunova;
 
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.concurrent.locks.AbstractOwnableSynchronizer;
+
+import javax.swing.JOptionPane;
 
 public class LK {
+
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("unesi prvo ime:");
-		String ime1 = scanner.nextLine();
-
-		System.out.println("unesi drugo ime:");
-		String ime2 = scanner.nextLine();
-
-		String s = ime1.toLowerCase() + ime2.toLowerCase();
-		System.out.println(s);
-
-		int[] niz = new int[ime1.length() + ime2.length()];
-		System.out.println(Arrays.toString(niz));
 		
-		
-		int b;
-		char z;
-		
-		for(int i=0;i<s.length();i++){
-			z=s.charAt(i);
-			b=0;
-			for(int j=0;j<s.length();j++) {
-				if(s.charAt(j)==z) {
-					b++;
-				}
-			}
-			niz[i]=b;
-			
-		}
-		System.out.println(Arrays.toString(niz));
-		
-		int polovina = niz.length / 2; //dodano
-		int[] noviNiz = new int[polovina]; //promijenjeno
-		for (int i = 0; i < polovina; i++) { //promijenjeno
-		    noviNiz[i] = niz[i] + niz[niz.length - 1 - i]; //promijenjeno
-		}
-		
-		System.out.println(Arrays.toString(noviNiz)); //promijenjeno
-		
-
-		
-		
-	}
-	
-	
-
-		public static int[] zbrajanjeNiza(int [] niz) { 
-		    int desniIndeks= niz.length-1;
-		    int lijeviIndeks=0;
-		    
-		    int[] noviNiz = new int[niz.length]; 
-		    int trenutniIndeks = 0; 
-		    
-		    while (lijeviIndeks <= desniIndeks) {
-		        int suma = niz[lijeviIndeks] + niz[desniIndeks]; 
-		        noviNiz[trenutniIndeks] = suma; 
-		        lijeviIndeks++;
-		        desniIndeks--;
-		        trenutniIndeks++; 
-		    }
-		    
-		    return noviNiz; 
-		    
-	    
-	    
-		
-	}
 }
--- eclipse/Edunova06/src/edunova/Ljubavni.java
+++ eclipse/Edunova06/src/edunova/Ljubavni.java
@@ -20,48 +20,5 @@ public class Ljubavni {
         String s = ime1 + ime2;
         System.out.println(s);
 
-        int[] niz = matrica1(s);
-        System.out.println(Arrays.toString(niz));
-
-        int[] zbrojeniNiz = zbrojiLijeveIDesneBrojeve(niz);
-        System.out.println(Arrays.toString(zbrojeniNiz));
-
-        scan.close(); 
-    }
-
-    public static int[] matrica1(String s) {
-        int[] niz = new int[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            char z = s.charAt(i);
-            int b = 0;
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == z) {
-                    b++;
-                }
-            }
-            niz[i] = b;
-        }
-        return niz;
-    }
-
-    public static int[] zbrojiLijeveIDesneBrojeve(int[] niz) {
-        int duljina = niz.length;
-        int[] zbrojeniNiz = new int[duljina / 2 + 1]; // Dodatni element ako je niz neparne duljine
-        int sredina = duljina / 2;
-
-        for (int i = 0; i <= sredina; i++) {
-            int lijevi = niz[i];
-            int desni = niz[duljina - 1 - i];
-
-            if (i == sredina && duljina % 2 != 0) {
-                // Ako je u sredini ostao jedan broj (za neparne duljine)
-                zbrojeniNiz[i] = lijevi;
-            } else {
-                zbrojeniNiz[i] = lijevi + desni;
-            }
-        }
-
-        return zbrojeniNiz;
-    }
+      
 }
