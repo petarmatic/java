70ca432342f9874b18710c4351eb8a6252ab7e6c
perom00 <petarmatic00@gmail.com>
2023-10-20 11:32:57 +0200

    Junit test failed

---
+++ eclipse/junit-5-basics/src/main/java/Unit/MathUtils.java
@@ -0,0 +1,8 @@
+package Unit;
+
+public class MathUtils {
+
+	public int add(int a, int b) {
+		return a*b;
+	}
+}
---
+++ eclipse/junit-5-basics/src/test/java/Unit/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package Unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
