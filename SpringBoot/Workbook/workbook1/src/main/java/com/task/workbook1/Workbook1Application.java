98785b565dd9df12b6d563e110b058dc280416d5
perom00 <petarmatic00@gmail.com>
2023-11-01 21:17:52 +0100

    workbook

---
+++ SpringBoot/Workbook/workbook1/src/main/java/com/task/workbook1/Workbook1Application.java
@@ -0,0 +1,13 @@
+package com.task.workbook1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workbook1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workbook1Application.class, args);
+	}
+
+}
