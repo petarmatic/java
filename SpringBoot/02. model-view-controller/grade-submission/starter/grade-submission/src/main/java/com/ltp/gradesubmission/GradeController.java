ff6233254aee8e16c60513db5648685ce50b1334
perom00 <petarmatic00@gmail.com>
2023-11-03 14:41:33 +0100

    thymeleaf,pojo,addAtributes

--- SpringBoot/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ SpringBoot/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,14 +1,20 @@
 package com.ltp.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
-
+    
     @GetMapping("/grades")
-    public String getGrades(){
+    private String getGrades(Model model){
+        Grade grade= new Grade("Harry", "Potions", "4");
+        Grade hermioneGrade = new Grade("Hermione", "Potions", "2");
+        model.addAttribute("grade",grade);
+        model.addAttribute("hermioneGrade", hermioneGrade);
         return "grades";
+
     }
     
 }
