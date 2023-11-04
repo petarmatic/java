1feb32cb144a39fc62c6af819c2f998740d7b5c9
perom00 <petarmatic00@gmail.com>
2023-11-04 21:58:05 +0100

    utility methods

--- SpringBoot/02. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ SpringBoot/02. model-view-controller/workbook 2.7/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -9,6 +9,7 @@ public class WorkbookController {
     
     @GetMapping("/")
     public String getName(Model model) {
+        model.addAttribute("name", "Petar");
         return "name-list";
     }
 }
