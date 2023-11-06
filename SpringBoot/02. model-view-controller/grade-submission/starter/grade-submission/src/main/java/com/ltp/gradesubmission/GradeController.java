2e1fd79d83e378444ad23c20ea351817388cbbe8
perom00 <petarmatic00@gmail.com>
2023-11-06 22:21:26 +0100

    update grade

--- SpringBoot/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ SpringBoot/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,6 @@
 package com.ltp.gradesubmission;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,27 +8,62 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
 
 
 
-    List<Grade> studentGrades= Arrays.asList(
-        new Grade("Harry", "Potions", "4"),
-        new Grade("Hermione", "Arithmany", "2"),
-        new Grade("Neville", "Charms", "2")
-    );
+    List<Grade> studentGrades= new ArrayList<>();
+       
         
   
+    @GetMapping("/")
+    public String getForm(Model model, @RequestParam(required =false) String name){
+       /* 
+        Grade grade;
+        if(getGradeIndex(name) == -1000){
+            grade=new Grade();
+        }else{
+            grade=studentGrades.get(getGradeIndex(name));
+        }
+       */ 
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
+        return "form";
+    }
+
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade){
+        int index=getGradeIndex(grade.getName());
+        if(index == -1000){
+            studentGrades.add(grade);
+        }else{
+            studentGrades.set(index, grade);
+        }
+        return "redirect:/grades";
+
+    }
+
     
     @GetMapping("/grades")
     private String getGrades(Model model){
-        
         model.addAttribute("grades", studentGrades);
-        
         return "grades";
 
     }
+
+    public Integer getGradeIndex(String name){
+        for (int i=0;i<studentGrades.size();i++){
+            if(studentGrades.get(i).equals(name)) return i;
+        }
+        return -1000;
+    }
+
+
+
+    
     
 }
