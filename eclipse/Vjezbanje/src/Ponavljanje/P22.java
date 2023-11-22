532dfdfa24b5ce0941db0c8737ddc87e914cc864
perom00 <petarmatic00@gmail.com>
2023-11-22 23:43:11 +0100

    Update P22.java

--- eclipse/Vjezbanje/src/Ponavljanje/P22.java
+++ eclipse/Vjezbanje/src/Ponavljanje/P22.java
@@ -1,10 +1,27 @@
 package Ponavljanje;
 
+import java.util.Scanner;
+
 public class P22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+	    System.out.println("Unesite brojeve");
+	    int broj = scanner.nextInt();
+	    int[] niz = new int[broj]; 
+		solution(niz);
+		
 
+	}
+	
+	public static int solution(int [] niz) {
+		
+        for (int i = 0; i < niz.length; i++) {
+           
+        }
+		
+		
+		return 0;
 	}
 
 }
