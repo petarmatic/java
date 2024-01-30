cecd6bf69a404a3bbfe1d687756f29e07707e801
perom00 <petarmatic00@gmail.com>
2024-01-30 22:03:21 +0100

    new

---
+++ SpringBoot/Excercise1/excercise1/src/test/java/com/excercise1/excercise1/Excercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise1.excercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Excercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
