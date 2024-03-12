d6db7b3febbca33cf0c490b499670797d317323c
Petar <petarmatic00@gmail.com>
2024-03-12 22:56:31 +0100

    Create P27.java

---
+++ eclipse/Vjezbanje/src/Ponavljanje/P27.java
@@ -0,0 +1,31 @@
+package Ponavljanje;
+
+import java.util.Scanner;
+
+public class P27 {
+
+	public static void main(String[] args) {
+		System.out.println("Disi pero");
+		
+		
+		Scanner unos= new Scanner(System.in);
+		System.out.println("Unesi nešto");
+		String uneseno= unos.next();
+		
+		nesto(uneseno);
+		
+		
+	}
+	
+	public static void nesto(String uneseno) {
+		
+		 if (uneseno.length() > 6) {
+	            System.out.println("Riječ ima više od 6 znakova.");
+	        } else {
+	            System.out.println("Riječ ima 6 ili manje znakova.");
+	        }
+		
+		
+	}
+
+}
