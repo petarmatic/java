967811ca09e2a802a7f717a51638fff190aa960e
Petar <petarmatic00@gmail.com>
2024-03-20 09:54:46 +0100

    new

--- SpringBoot/FirstSpringBoot/src/main/java/fishingstore/HelloController.java
+++ SpringBoot/FirstSpringBoot/src/main/java/fishingstore/HelloController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package example.springboot;
+package fishingstore;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
