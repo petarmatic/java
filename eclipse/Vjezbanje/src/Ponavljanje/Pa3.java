46edbbeda98c3a094fbcf71294348b690fa069f1
Petar <petarmatic00@gmail.com>
2024-04-28 20:00:28 +0200

    Create Pa3.java

---
+++ eclipse/Vjezbanje/src/Ponavljanje/Pa3.java
@@ -0,0 +1,28 @@
+package Ponavljanje;
+
+import java.util.Scanner;
+
+public class Pa3 {
+
+	public static void main(String[] args) {
+		
+	        System.out.println("Input the value of n: ");
+	        
+	        Scanner in = new Scanner(System.in);
+	        
+	        int n = in.nextInt();
+	        
+	        while (n != 1) {
+	            if (n % 2 == 0) {
+	                n = n / 2; 
+	            } else {
+	                n = (3 * n + 1) / 2; 
+	            }
+	        }
+	        
+	        System.out.println("\nValue of n = " + n);
+	        
+	        in.close();
+	}
+
+}
