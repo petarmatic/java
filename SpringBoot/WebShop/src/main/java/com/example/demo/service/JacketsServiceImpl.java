348731d66548fd70d86003b650fce6b6f7ecf156
Petar <petarmatic00@gmail.com>
2024-05-06 10:08:27 +0200

    new

--- SpringBoot/WebShop/src/main/java/com/example/demo/service/JacketsServiceImpl.java
+++ SpringBoot/WebShop/src/main/java/com/example/demo/service/JacketsServiceImpl.java
@@ -2,6 +2,9 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Jackets;
+import com.example.demo.repository.JacketsRepository;
+
 import lombok.AllArgsConstructor;
 
 
@@ -9,7 +12,28 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
-public class JacketsServiceImpl {
+public class JacketsServiceImpl implements JacketsService{
+
+    JacketsRepository jacketsRepository;
+
+    @Override
+    public Jackets getJackets(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getJackets'");
+    }
+
+    @Override
+    public Jackets saveJackets(Jackets jackets) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveJackets'");
+    }
+
+    @Override
+    public void deleteJackets(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteJackets'");
+    }
+
     
 
 
