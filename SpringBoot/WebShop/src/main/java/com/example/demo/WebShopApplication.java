9ffff2b004767526de3520c30e47578e0398409b
Petar <petarmatic00@gmail.com>
2024-05-22 22:11:58 +0200

    Update WebShopApplication.java

--- SpringBoot/WebShop/src/main/java/com/example/demo/WebShopApplication.java
+++ SpringBoot/WebShop/src/main/java/com/example/demo/WebShopApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.repository.JacketsRepository;
+import com.example.demo.repository.PantsRepository;
+import com.example.demo.repository.ShirtsRepository;
+
+
+
+
+
 @SpringBootApplication
-public class WebShopApplication {
+public class WebShopApplication implements CommandLineRunner {
+
+
+	@Autowired
+	JacketsRepository jacketsRepository;
+
+	@Autowired
+	PantsRepository pantsRepository;
+
+	@Autowired
+	ShirtsRepository shirtsRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
