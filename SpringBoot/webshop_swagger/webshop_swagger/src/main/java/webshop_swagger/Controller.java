3dc4aee3d48a0066dcaec11d21b26985d90a24ed
Petar <petarmatic00@gmail.com>
2024-06-23 16:43:06 +0200

    new

--- SpringBoot/webshop_swagger/webshop_swagger/src/main/java/webshop_swagger/Controller.java
+++ SpringBoot/webshop_swagger/webshop_swagger/src/main/java/webshop_swagger/Controller.java
@@ -1,8 +1,49 @@
 package webshop_swagger;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
     
+     @GetMapping("/get")
+    public ResponseEntity<String> get(){
+        return new ResponseEntity<>("Dobrodošli na webshop",HttpStatus.OK);
+    }
+    
+    
+    @PostMapping("/post")
+    public ResponseEntity<String> post(
+    @RequestParam (required = true) String naziv,
+    @RequestParam (required = false) int broj
+    ){
+        return new ResponseEntity<>("Primio " + naziv + " i " + broj,HttpStatus.CREATED);
+    }
+    
+    @PutMapping("/put")
+    public ResponseEntity<String> put(
+    @RequestParam (required = true) int sifra,
+    @RequestParam (required = true) String naziv,
+    @RequestParam (required = false) int broj
+    ){
+        return new ResponseEntity<>("Promjenjeno "+ naziv + " i " + broj + " na " + sifra,HttpStatus.OK);
+    }
+    
+     @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(
+    @RequestParam (required = true) int sifra
+    ){
+        return new ResponseEntity<>("Obrisano " + sifra,HttpStatus.OK);
+    }
 
     
 }
--- SpringBoot/webshop_swagger/webshop_swagger/src/main/java/webshop_swagger/MyConfiguration.java
+++ SpringBoot/webshop_swagger/webshop_swagger/src/main/java/webshop_swagger/MyConfiguration.java
@@ -1,8 +1,28 @@
 package webshop_swagger;
 
-public class MyConfiguration{
-    
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+    
+    @Bean
+  public OpenAPI myOpenAPI() {
+    Server devServer = new Server();
+    devServer.setUrl("http://localhost:8080");
+    devServer.setDescription("Razvoj (DEV)");
+
+    Server prodServer = new Server();
+    prodServer.setUrl("https://jsb.unixoidi.xyz");
+    prodServer.setDescription("Produkcija (PROD)");
+
+    
+    return new OpenAPI().servers(List.of(devServer, prodServer));
+  }
     
   
 }
